86dd636874c7aa30500c9e7be38bfdde0eb6234a
RayPas <70732899+RayPas@users.noreply.github.com>
2022-12-29 20:49:02 -0800

    Button add

--- react-template/src/components/Description.js
+++ react-template/src/components/Description.js
@@ -1,9 +1,8 @@
 function Description() {
     return (
         <div>
-            <p>How it works: Start off by loggin in or creating an account!</p>
-            <p>Start by selecting your trip dates</p>
-            <p>Use our search engine to plan your trip out and enjoy!</p>
+            <p>Plan your trip!</p>
+            <button></button>
         </div>
     )
 }
