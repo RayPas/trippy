2becb5c4123725e986a7025cb12f44475eaa2a52
RayPas <70732899+RayPas@users.noreply.github.com>
2023-01-27 13:47:41 -0800

    Adjusted the folders

--- react-template/src/components/Activity Tracker/index.js
+++ react-template/src/components/Activity Tracker/index.js
@@ -1,25 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const ActivityTracker = () => {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
+  const [activites, setActivites] = useState([]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     // Store the inputValue
-    console.log(inputValue);
+    setActivites([...activites, inputValue]);
+    // console.log(inputValue);
+    // console.log(activites)
     // Clear the input field
-    setInputValue('');
+    setInputValue("");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div>
+        <table>
+          <ul>
+            {activites.map((activity) => {
+              return <li>{activity}</li>;
+            })}
+          </ul>
+        </table>
+      </div>
+    </div>
   );
 };
 
